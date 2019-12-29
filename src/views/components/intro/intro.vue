a87a7582e2399b5168ac2426c9dd80a346be5592
Shiotsuki_ <564165781@qq.com>
2019-12-29 12:44:33 +0800

    feat：增加组件介绍页面

---
+++ src/views/components/intro/intro.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="components-intro markdown">
+        <Markdown></Markdown>
+    </div>
+</template>
+
+<script>
+import Markdown from './markdown/intro.md'
+export default {
+  name: 'components-demo',
+  mounted () {
+
+  },
+  components: {
+    Markdown
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
