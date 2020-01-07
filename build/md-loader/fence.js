203f62f5f048e7a5ac1f3773fb814b07fd998a9a
zhouyuan <zhouyuan@minstone.com.cn>
2020-01-07 15:17:36 +0800

    feat：增加demoBox代码兼容无script或者无style

--- build/md-loader/fence.js
+++ build/md-loader/fence.js
@@ -13,6 +13,27 @@ let highlight = (str, lang) => {
               '</code></pre>'
   }
 }
+// demo高亮
+let demoHighlight = (content) => {
+  // 需要区分css，js，html的高亮
+  // 利用md-loader提供的工具函数
+  let html = highlight(stripTemplate(content), 'html')
+  let script = highlight(stripScript(content), 'js').slice(24, -13)
+  let css = highlight(stripStyle(content), 'css').slice(24, -13)
+  let container = `${html}`
+  if (script.trim()) {
+    container = container.slice(0, -13) + '<br><br><span class="hljs-tag">&lt;script&gt;</span><br>' +
+    script +
+    '<br><span class="hljs-tag">&lt;/script&gt;</span></code></pre>'
+  }
+  if (css.trim()) {
+    container = container.slice(0, -13) + '<br><br><span class="hljs-tag">&lt;style&gt;</span><br>' +
+    css +
+    '<br><span class="hljs-tag">&lt;/style&gt;</span></code></pre>'
+  }
+  
+return `<template slot="highlight">${container}</template>`
+}
 // 覆盖默认的 fence 渲染策略
 module.exports = md => {
   const defaultRender = md.renderer.rules.fence;
@@ -23,19 +44,8 @@ module.exports = md => {
     const isInDemoContainer = prevToken && prevToken.nesting === 1 && prevToken.info.trim().match(/^demo\s*(.*)$/);
     if (token.info.trim() === 'html' && isInDemoContainer) {
         // 在node层直接进行高亮渲染
-        // 需要区分css，js，html的高亮
-        // 利用md-loader提供的工具函数
-        let html = highlight(stripTemplate(token.content), 'html').slice(0, -13)
-        let script = highlight(stripScript(token.content), 'js').slice(24, -13)
-        let css = highlight(stripStyle(token.content), 'css').slice(24, -13)
-        let container = `${html}<br><br>` +
-        '<br><span class="hljs-tag">&lt;script&gt;</span><br>' +
-        script +
-        '<br><span class="hljs-tag">&lt;/script&gt;</span><br><br>'+
-        '<br><span class="hljs-tag">&lt;style&gt;</span><br>' +
-        css +
-        '<br><span class="hljs-tag">&lt;/style&gt;</span><br></code></pre>'
-      return `<template slot="highlight">${container}</template>`;
+        return demoHighlight(token.content)
+
     } else if (!!token.info && token.info !== " ") {
       // 不是展示代码的code也加上高亮
       return highlight(token.content, token.info.trim())
