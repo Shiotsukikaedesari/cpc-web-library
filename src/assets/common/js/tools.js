63bb9e31527695a54f7991b75a2feb9a980f6143
Shiotsuki_ <564165781@qq.com>
2020-01-24 17:07:54 +0800

    feat：增加clearWhitespace公告方法

--- src/assets/common/js/tools.js
+++ src/assets/common/js/tools.js
@@ -1,6 +1,15 @@
 const Tools = {
-  printVue () {
-    console.log(123)
+  /**
+   * @param {dom} element dom
+   * @description 清楚dom节点的空白节点
+   */
+  cleanWhitespace (element) {
+    for (let i = 0; i < element.childNodes.length; i += 1) {
+      let node = element.childNodes[i]
+      if (node.nodeType === 3 && !/\S/.test(node.nodeValue)) {
+        node.parentNode.removeChild(node)
+      }
+    }
   }
 }
 
