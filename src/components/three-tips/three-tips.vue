93903ae0399adf147e0f0b3de31aff5eb4ef62ca
Shiotsuki_ <564165781@qq.com>
2020-03-05 18:03:56 +0800

    feat：three-tip插件修改

--- src/components/three-tips/three-tips.vue
+++ src/components/three-tips/three-tips.vue
@@ -19,15 +19,13 @@ export default {
   position: fixed;
   top: 0;
   left: 50%;
-  margin-left: -200px;
-  width: 400px;
+  margin-left: -20%;
+  width: 40%;
   padding: 10px;
   color: white;
   text-shadow: 0 0 4px rgb(68, 68, 68);
   text-align: center;
-  border: 1px solid white;
-  border-top: 0;
-  box-shadow: 0 0 6px rgb(255, 112, 255);
+  box-shadow: 0 0 8px rgb(168, 168, 168);
   white-space:pre-wrap;
 }
 </style>
