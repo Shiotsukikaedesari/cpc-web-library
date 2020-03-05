3d3522a847da3c7f7ffed4ebd6d2b527260be820
Shiotsuki_ <564165781@qq.com>
2020-03-05 13:46:35 +0800

    pref：修改导航布局

--- src/views/three.vue
+++ src/views/three.vue
@@ -3,12 +3,29 @@
     <div class="cpc-nav-side">
         <cpc-nav-side :width="sideNavWidth" height="100%" background="">
           <template slot="menu">
-                <cpc-nav-side-elem elemKey="0" slot="child" :click="toIndex">首页</cpc-nav-side-elem>
-                <cpc-nav-side-elem elemKey="1" slot="child" :isSignal="true" :click="toInitThree">初始场景辅助</cpc-nav-side-elem>
-                <cpc-nav-side-elem elemKey="2" slot="child" :click="toWatchKeyframe">帧率监视器</cpc-nav-side-elem>
-                <cpc-nav-side-elem elemKey="3" slot="child" :click="toCameraFollowMouse">相机跟随鼠标</cpc-nav-side-elem>
-                <cpc-nav-side-elem elemKey="4" slot="child" :click="toAutoRotateCamera">相机自动旋转</cpc-nav-side-elem>
-                <cpc-nav-side-elem elemKey="5" slot="child" :click="toDragCamera">相机交互插件（OrbitControls）</cpc-nav-side-elem>
+            <cpc-nav-side-elem elemKey="0" slot="father" :click="toIndex">首页</cpc-nav-side-elem>
+            <cpc-nav-side-menu menuKey="1" slot="menu" :open="true">
+              <cpc-nav-side-elem elemKey="1-0" slot="father" :father="true" isSignal>
+                <cpc-icon slot="side-ui" code="#icon-arrow-right-drop-circle"></cpc-icon>开始
+              </cpc-nav-side-elem>
+              <cpc-nav-side-elem elemKey="1-1" slot="child" :isSignal="true" :click="toInitThree">场景初始</cpc-nav-side-elem>
+              <cpc-nav-side-elem elemKey="1-2" slot="child" :click="toWatchKeyframe">帧率监视器（stats）</cpc-nav-side-elem>
+              <cpc-nav-side-elem elemKey="1-3" slot="child" :click="toInitObj">初始物体</cpc-nav-side-elem>
+            </cpc-nav-side-menu>
+            <cpc-nav-side-menu menuKey="2" slot="menu">
+              <cpc-nav-side-elem elemKey="2-0" slot="father" :father="true">
+                <cpc-icon slot="side-ui" code="#icon-camera"></cpc-icon>相机
+              </cpc-nav-side-elem>
+              <cpc-nav-side-elem elemKey="2-1" slot="child" :click="toCameraFollowMouse">相机跟随鼠标</cpc-nav-side-elem>
+              <cpc-nav-side-elem elemKey="2-2" slot="child" :click="toAutoRotateCamera">相机自动旋转</cpc-nav-side-elem>
+              <cpc-nav-side-elem elemKey="2-3" slot="child" :click="toDragCamera">相机交互插件（OrbitControls）</cpc-nav-side-elem>
+            </cpc-nav-side-menu>
+            <cpc-nav-side-menu menuKey="3" slot="menu">
+              <cpc-nav-side-elem elemKey="3-0" slot="father" :father="true">
+                <cpc-icon slot="side-ui" code="#icon-camera"></cpc-icon>物体
+              </cpc-nav-side-elem>
+              <cpc-nav-side-elem elemKey="3-1" slot="child" :click="toControlObj">物体交互控件（TransformControls）</cpc-nav-side-elem>
+            </cpc-nav-side-menu>
             </template>
         </cpc-nav-side>
     </div>
@@ -41,6 +58,10 @@ export default {
     toWatchKeyframe () {
       this.$router.replace({path: '/three/watchKeyframe'})
     },
+    // 初始物体
+    toInitObj () {
+      this.$router.replace({path: '/three/initObj'})
+    },
     // 相机跟随鼠标
     toCameraFollowMouse () {
       this.$router.replace({path: '/three/cameraFollowMouse'})
@@ -52,6 +73,10 @@ export default {
     // 相机拖动旋转
     toDragCamera () {
       this.$router.replace({path: '/three/dragCamera'})
+    },
+    // 物体交互插件
+    toControlObj () {
+      this.$router.replace({path: '/three/controlObj'})
     }
   },
   computed: {
@@ -75,10 +100,21 @@ export default {
         >  .container {
           background: transparent;
           > .container  {
+            /deep/ .father-elem {
+              > .container {
+                background: transparent;
+                .icon-container {
+                  color: white;
+                }
+              }
+            }
+            /deep/ .child-elem {
+              > .container {
+                background: transparent;
+              }
+            }
             background: transparent;
-            border-bottom: 1px solid rgb(214, 214, 214);
             /deep/ .elem-box {
-              // background: white;
               > .left-box {
                 > .ui {color: white;}
                 > .title {color: white;}
