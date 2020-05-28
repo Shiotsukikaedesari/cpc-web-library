58eb54e6ef7aad4bae5808678e1f427e7c4f6942
shiotsuki_ <564165781@qq.com>
2020-05-28 23:05:57 +0800

    pref：优化打包结构，调整部分样式与功能

--- src/views/notes.vue
+++ src/views/notes.vue
@@ -6,7 +6,7 @@
                   <cpc-nav-side-elem elemKey="1-0" slot="father" :father="true" :isSignal="true" :arrowInit="true">
                     <cpc-icon slot="side-ui" code="#icon-arrow-right-drop-circle"></cpc-icon>开始
                   </cpc-nav-side-elem>
-                  <cpc-nav-side-elem elemKey="1-1" slot="child" :isSignal="true" :click="toNotesIntro">
+                  <cpc-nav-side-elem elemKey="1-1" slot="child" :isSignal="true" :click="jump('intro')">
                     <cpc-icon slot="side-ui" code="#icon-file-document-box" size="20px"></cpc-icon>基本介绍
                   </cpc-nav-side-elem>
               </cpc-nav-side-menu>
@@ -14,17 +14,20 @@
                   <cpc-nav-side-elem elemKey="2-0" slot="father" :father="true">
                     <cpc-icon slot="side-ui" code="#icon-language-html"></cpc-icon>HTML
                   </cpc-nav-side-elem>
-                  <cpc-nav-side-elem elemKey="2-1" slot="child" :click="toHtmlBase">HTML基础</cpc-nav-side-elem>
-                  <cpc-nav-side-elem elemKey="2-2" slot="child" :click="toHtmlTreasure">HTML宝典</cpc-nav-side-elem>
-                  <cpc-nav-side-elem elemKey="2-3" slot="child">组件三</cpc-nav-side-elem>
+                  <cpc-nav-side-elem elemKey="2-1" slot="child" :click="jump('html-base')">HTML基础</cpc-nav-side-elem>
+                  <cpc-nav-side-elem elemKey="2-2" slot="child" :click="jump('html-treasure')">HTML宝典</cpc-nav-side-elem>
               </cpc-nav-side-menu>
               <cpc-nav-side-menu menuKey="3" slot="menu">
                 <cpc-nav-side-elem elemKey="3-0" slot="father" :father="true">
                   <cpc-icon slot="side-ui" code="#icon-package-variant"></cpc-icon>CSS
                 </cpc-nav-side-elem>
-                  <cpc-nav-side-elem elemKey="3-1" slot="child">组件一</cpc-nav-side-elem>
-                  <cpc-nav-side-elem elemKey="3-2" slot="child">组件二</cpc-nav-side-elem>
-                  <cpc-nav-side-elem elemKey="3-3" slot="child">组件三</cpc-nav-side-elem>
+              </cpc-nav-side-menu>
+              <cpc-nav-side-menu menuKey="4" slot="menu">
+                  <cpc-nav-side-elem elemKey="4-0" slot="father" :father="true">
+                    <cpc-icon slot="side-ui" code="#icon-language-html"></cpc-icon>webpack
+                  </cpc-nav-side-elem>
+                  <cpc-nav-side-elem elemKey="4-1" slot="child" :click="jump('webpack-base')">webpack基础</cpc-nav-side-elem>
+                  <cpc-nav-side-elem elemKey="4-2" slot="child" :click="jump('webpack-senior')">webpack高级</cpc-nav-side-elem>
               </cpc-nav-side-menu>
           </cpc-nav-side>
         </div>
@@ -45,17 +48,12 @@ export default {
     }
   },
   methods: {
-    // 跳转到介绍
-    toNotesIntro () {
-      this.$router.push({path: '/notes/intro'})
-    },
-    // 跳转到html基础
-    toHtmlBase () {
-      this.$router.push({path: '/notes/html-base'})
-    },
-    // 跳转到html宝典
-    toHtmlTreasure () {
-      this.$router.push({path: '/notes/html-treasure'})
+    // 路由跳转
+    jump (path) {
+      function jump () {
+        this.$router.replace({path: `/notes/${path}`})
+      }
+      return jump
     }
   },
   created () {
