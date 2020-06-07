3355fedd442935c46cfad21f2d5e28990113ce46
shiotsuki_ <564165781@qq.com>
2020-06-07 16:12:49 +0800

    pref：优化webpack打包

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -9,7 +9,6 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
